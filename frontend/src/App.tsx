26bda75b171a6f4371c111d0d65e4f61ab4ffed3
Rongrui Zhu <ronrzhu@gmail.com>
2025-11-11 22:29:58 -0800

    Renamed backend to be SeattleNow

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout';
+import { Home } from './pages/Home';
 
 export function App() {
   return (
     <BrowserRouter basename="/DubPolls">
       <Routes>
         <Route element={<Layout />}>
-          <Route path="/" element={<div>Hello World!</div>} />
+          <Route path="/" element={<Home />} />
           <Route path="/following" element={<div>Following</div>} />
           <Route path="/account" element={<div>Account</div>} />
           <Route path="/settings" element={<div>Settings</div>} />
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,12 @@
+export function Home() {
+    return (
+        <div>
+            <div className="h-[60px] border-b border-neutral-600">
+                
+            </div>
+            <div>
+                Hello World!
+            </div>
+        </div>
+    )
+}
